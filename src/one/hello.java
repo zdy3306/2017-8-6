2c2a8e62511005eb58179dd4028cd857864a8cb1
win_gj <zdy3306@163.com>
2017-08-06 16:40:57 +0800

    方法更改

--- src/one/hello.java
+++ src/one/hello.java
@@ -3,5 +3,9 @@ package one;
 public class hello {
     public static void main(String[] args) {
         System.out.printf("git");
+        System.out.printf("hub");
+    }
+    public void say(){
+        System.out.printf("四川.成都");
     }
 }
